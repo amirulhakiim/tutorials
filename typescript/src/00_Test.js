3e98fc5f088c65e54820284c4bab8898d36a7da5
Amirul Hakim <amirulhakimazmi@gmail.com>
2022-02-15 14:40:18 +0800

    Add typescript tutorial

---
+++ typescript/src/00_Test.js
@@ -0,0 +1,5 @@
+var message = 'Hello World';
+console.log(message);
+var str = '1';
+var str2 = str;
+console.log('str2 is of type ' + typeof (str2));
---
+++ typescript/src/00_Test.ts
@@ -0,0 +1,6 @@
+var message:string = 'Hello World'
+console.log(message)
+
+var str = '1' 
+var str2:number = <number> <any> str   //str is now of type number 
+console.log('str2 is of type ' + typeof(str2))
